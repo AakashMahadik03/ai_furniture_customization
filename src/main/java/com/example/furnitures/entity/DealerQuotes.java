26bb255c6a7afad81414b1a1bef40c29da98c038
Sreekanth K <kanth5994@gmail.com>
2025-02-19 00:06:02 +0530

    Added DealerQuotes Entity

--- src/main/java/com/example/furnitures/entity/DealerQuotes.java
+++ src/main/java/com/example/furnitures/entity/DealerQuotes.java
@@ -1,7 +1,31 @@
 package com.example.furnitures.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "DealerQuotes")
 public class DealerQuotes {
 
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private int DealerQuotesId;
+	
+	private Feasibility feasibility;
+	
+	private Merchant merchant;
+	
+	private int price;
+	
+	private Date estimatedDelivery;
+	
 
 }
