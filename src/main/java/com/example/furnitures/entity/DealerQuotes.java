41fa02a4c75949163ef57e58679197277fd1375d
Sreekanth K <kanth5994@gmail.com>
2025-02-19 00:21:42 +0530

    Updated DealerQuotes

--- src/main/java/com/example/furnitures/entity/DealerQuotes.java
+++ src/main/java/com/example/furnitures/entity/DealerQuotes.java
@@ -2,12 +2,13 @@ package com.example.furnitures.entity;
 
 import java.util.Date;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -20,7 +21,7 @@ public class DealerQuotes {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private int DealerQuotesId;
 	
-	@OneToOne
+	@ManyToAny
 	private Feasibility feasibility;
 	
 	private Merchant merchant;
